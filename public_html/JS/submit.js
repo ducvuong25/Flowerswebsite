03b83fcf0125dd519a2c90f7e87c95fc9f6d9234
ducvuong25 <ducvuong25@gmail.com>
2020-10-10 11:10:10 +0700

    add inital file and folder

---
+++ public_html/JS/submit.js
@@ -0,0 +1,5 @@
+                function thongbao() {
+                    var name = document.getElementById('name').value;
+                    var email = document.getElementById('email').value;
+                    alert("Bạn tên là: " + name + "\nEmail: " + email);
+                }
